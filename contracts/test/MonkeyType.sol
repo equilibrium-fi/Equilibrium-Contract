6a89279857e1380ef7ab54f1c4b0c173c9744d41
Aris <2765005260@qq.com>
2025-12-21 17:04:20 +0800

    用于测试的ERC20Token

---
+++ contracts/test/MonkeyType.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title luna test ERC20Token contract 
+ * @author @sjhana(github)
+ * @notice 用于测试的ERC20Token
+ */
+
+contract MonkeyType is ERC20 {
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
+
+    function mint(address to, uint256 value) external {
+        _mint(to, value);
+    }
+}
