aa6a069a096f4db9f3a7bc6973322fd32bce34a6
Aris <2765005260@qq.com>
2025-12-23 16:44:31 +0800

    增加了forking和RPC_URL配置

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "dotenv/config";
+
+const RPC_URL = process.env.RPC_URL!;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      forking: {
+        url: RPC_URL,
+        blockNumber: 24066007
+      }
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    }
+  }
 };
 
 export default config;
